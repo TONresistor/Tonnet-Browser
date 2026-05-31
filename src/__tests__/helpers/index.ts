29b501d5ceb85abca80aff4a0aef41bea9b52d01
TONresistor <armed-aloha-rely@duck.com>
2026-05-31 23:20:27 +0200

    test: remove unused test-helper library and dead @tests alias

--- src/__tests__/helpers/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Test helpers - single import point
- * Usage: import { createMockBrowserWindow, createMockTab, useFakeTimers } from '@tests/helpers'
- */
-
-export * from './mocks'
-export * from './factories'
-export * from './setup'
